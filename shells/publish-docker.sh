3c2cf29e71fc6fd3ce3affe0fb819707d9435d44
snltty <iswwping@163.com>
2026-01-07 10:39:54 +0800

    net10

--- shells/publish-docker.sh
+++ shells/publish-docker.sh
@@ -14,7 +14,7 @@ do
     for r in ${rs[@]} 
     do
         rr=linux-${r}
-        dotnet publish ${f} -c release -f net8.0 -o public/publish/docker/linux-${r}/${f} -r ${rr}  -p:PublishSingleFile=true  --self-contained true  -p:TrimMode=partial -p:TieredPGO=true  -p:DebugType=full -p:EventSourceSupport=false -p:DebugSymbols=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true -p:MetricsSupport=false -p:StackTraceSupport=false -p:XmlResolverIsNetworkingEnabledByDefault=false
+        dotnet publish ${f} -c release -f net10.0 -o public/publish/docker/linux-${r}/${f} -r ${rr}  -p:PublishSingleFile=true  --self-contained true  -p:TrimMode=partial -p:TieredPGO=true  -p:DebugType=full -p:EventSourceSupport=false -p:DebugSymbols=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true -p:MetricsSupport=false -p:StackTraceSupport=false -p:XmlResolverIsNetworkingEnabledByDefault=false
         cp -rf ${f}/Dockerfile public/publish/docker/linux-${r}/${f}/Dockerfile
         cp -rf public/extends/any/web public/publish/docker/linux-${r}/${f}/web
         mkdir -p public/publish/docker/linux-${r}/${f}/projects
