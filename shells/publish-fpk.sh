08a0ad019ba64f2708b2d2ec66d495e404f719a2
snltty <iswwping@163.com>
2026-01-24 15:27:59 +0800

    1.0.3

--- shells/publish-fpk.sh
+++ shells/publish-fpk.sh
@@ -3,9 +3,9 @@ target=$(cd $(dirname $0)/..; pwd)
 mkdir -p public/publish-fpk/docker
 cp -rf install-package/fpk/docker/* public/publish-fpk/docker
 
-sed -i "s|{version}|1.0.2|g" public/publish-fpk/docker/manifest
+sed -i "s|{version}|1.0.3|g" public/publish-fpk/docker/manifest
 sed -i "s|{changelog}|1. 一些累计更新，写了一些新bug、2. 一些bug优化、3. ssl证书|g" public/publish-fpk/docker/manifest
-sed -i "s|{version}|v1.0.2|g" public/publish-fpk/docker/app/docker/docker-compose.yaml
+sed -i "s|{version}|v1.0.3|g" public/publish-fpk/docker/app/docker/docker-compose.yaml
 sed -i 's/\r$//' public/publish-fpk/docker/manifest
 sed -i 's/\r$//' public/publish-fpk/docker/cmd/main
 sed -i 's/\r$//' public/publish-fpk/docker/cmd/uninstall_callback
